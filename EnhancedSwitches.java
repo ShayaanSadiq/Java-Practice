b7430e468d65e0df5f4d09815e6d8f1b990cc3cf
Shayaan Sadiq <shayaansadiq1402@gmail.com>
2026-03-01 05:51:03 +0530

    Third recorded day

---
+++ EnhancedSwitches.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EnhancedSwitches {
+    public static void main(String[] args) {
+        
+        // Enhanced Switch = a combination of else if statements
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        String day;
+
+        System.out.print("Enter the day of the week: ");
+        day = scanner.nextLine();
+
+        switch(day) {
+            case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" -> System.out.println("It is a weekday 🤬");
+            case "Saturday", "Sunday" -> System.out.println("It is the weekend 🥳");
+            default -> System.out.println(day + " is not a day!");
+        }
+
+        scanner.close();
+    }
+}
