3a490da1ec66ffd9731751e09d39c7538feb4bba
Shayaan Sadiq <shayaansadiq1402@gmail.com>
2026-03-02 06:34:35 +0530

    Fourth Recorded Day

---
+++ toString/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Car car = new Car("Ford", "Mustang", "Yellow", 2025);
+
+        System.out.println(car);
+        
+        car.toString();
+    }
+}
