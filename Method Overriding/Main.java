314970c16ec873a02ae86d2c034ae0e37464535b
Shayaan Sadiq <shayaansadiq1402@gmail.com>
2026-03-02 06:32:55 +0530

    Fourth Recorded Day

---
+++ Method Overriding/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+
+        cat.move();
+        dog.move();
+        fish.move();
+    } 
+}
