e1a33299510d18a901d577c4b49f88b6317d8a77
Shayaan Sadiq <shayaansadiq1402@gmail.com>
2026-03-12 12:52:14 +0530

    Sixth Recorded Day

---
+++ Reading from files/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\shaya\\Documents\\coding practice\\Writing to files\\test.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found.");
+        } catch (IOException e) {
+            System.out.println("Something went wrong with Input/Output");
+        }
+
+    }
+}
