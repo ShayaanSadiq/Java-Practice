b863f27ac6e4dde36b8efb2433592a23fd934825
Shayaan Sadiq <shayaansadiq1402@gmail.com>
2026-03-20 14:59:15 +0530

    Final Recorded Day

---
+++ MultiThreading/Multithread.java
@@ -0,0 +1,22 @@
+public class Multithread {
+    public static void main(String[] args) {
+        
+        Thread thread1 = new Thread(new MyRunnable2("ping"));
+        Thread thread2 = new Thread(new MyRunnable2("pong"));
+
+        System.out.println("Game start!");
+
+        thread1.start();
+        thread2.start();
+        
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main Thread was interrupted.");
+        }
+
+        System.out.println("Game over!");
+
+    }
+}
